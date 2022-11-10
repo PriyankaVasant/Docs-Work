c7fad42ff2a66d75146c01fa0f28e37118bfbed4
priyanka Vasant <priyanka.vasant@mindinventory.com>
2022-11-10 11:19:40 +0530

    Add project

---
+++ DOcsWork/DOcsWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DOcsWork
+//
+//  Created by mac-0009 on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
