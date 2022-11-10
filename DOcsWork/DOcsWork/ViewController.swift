8a42979c42c6b1f6be28245997cebccfa79dd6d8
priyanka Vasant <priyanka.vasant@mindinventory.com>
2022-11-10 18:28:15 +0530

    test

--- DOcsWork/DOcsWork/ViewController.swift
+++ DOcsWork/DOcsWork/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world...!!asdasd  ")
+        print("hello world...!!")
         // Do any additional setup after loading the view.
     }
 
